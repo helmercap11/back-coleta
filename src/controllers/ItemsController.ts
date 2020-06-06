7e0a9be777a66631c14e7bc4850c5d70a9952d49
helmercap11 <helmercap@gmail.com>
2020-06-06 22:18:42 +0100

    create upload and install multer

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3000/uploads/${item.image}`,
+                image_url: `http://192.168.1.3:3000/uploads/${item.image}`,
             };
         });
     
--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -20,7 +20,14 @@ class PointsController {
             .select('points.*');
 
 
-        return response.json(points);
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.1.3:3000/uploads/${point.image}`,
+            };
+        });
+    
+        return response.json(serializedPoints);
     }
 
     async show(request: Request, response: Response) {
@@ -32,12 +39,19 @@ class PointsController {
             return response.status(400).json( {  message: 'Point not found' });
         }
 
+
+        const serializedPoint =  {
+                ...point,
+                image_url: `http://192.168.1.3:3000/uploads/${point.image}`,
+        };
+    
+
         const items = await knex('items')   
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return response.json({ point, items });
+        return response.json({ point: serializedPoint, items });
     }
 
     async create (request: Request, response: Response) {
@@ -56,21 +70,24 @@ class PointsController {
    
 
         const point = {
-            image: 'image Fake',
+            image: request.file.filename,
             name,
             email, 
             whatsapp,
-            latitude,
-            longitude,
-            city,
+            latitude,   
+            longitude,   
+            city,  
             uf
          };
 
         const insertedIds = await trx('points').insert(point);
    
         const point_id = insertedIds[0];
-        const pointItems = items.map((item_id: number) => {
-           return {
+        const pointItems = items
+            .split(',') 
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+            return {
                item_id,
                point_id,
            }
